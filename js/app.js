6671f69428ba719aff3b017e3de9b96afeca9d41
bloneeee <blone5128@gmial.com>
2023-01-12 18:19:49 +0630

    12 Jan 2023 6PM

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 // jQuery Area
 
 $(document).ready(function(){
-    // console.log('hi');
+    // Start Left Sidebar Section
+
+    // start left sidebar
+
+    $(".sidebarlinks").click(function(){
+        $('.sidebarlinks').removeClass('currents');
+        $(this).addClass("currents");
+    });
+
+    // end left sidebar
+
+    // End Left Sidebar Section
 });
 
 // Javascript Area
